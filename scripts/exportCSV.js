c09605c3d22175cc058b935edf4c60ab177ed29c
Darion01 <dariusgh01@yahoo.com>
2022-06-21 15:51:16 +0300

    CSV

---
+++ scripts/exportCSV.js
@@ -0,0 +1,29 @@
+function exportPerfumes()
+{
+    let CSVfile = 'data:text/csv;charset=utf-8,name,season,gender,smell,category,initial_stock,current_stock\n';
+    console.log("Merge");
+    fetch("/get-products", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      
+      }).then(response => {
+        // Converting received data to JSON
+        
+        return response.json();
+      }).then(json => {
+      
+        // Loop through each data
+        for(var i = json.length - 1; i >= 0; i--) {
+          var product = json[i];
+          console.log("Merge???");
+          CSVfile += `${product.name},${product.season},${product.gender},${product.smell},${product.category},${product.initialstock},${product.quantity}\n`
+        }
+
+        window.alert(CSVfile)
+
+      }).catch(err => {
+        console.log(err)
+      })
+}
